bd6dd46aff53e4348db11ed9f6b17ce1330a2dc2
patrick <iampratik70@gmail.com>
2024-09-12 23:16:42 +0530

    fetched data from db as ticket header

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,4 +1,4 @@
-import Ticket from "../../(models)/Ticket";
+import Ticket from "@/app/(models)/Ticket";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
@@ -12,6 +12,16 @@ export async function POST(req) {
       { message: "Ticket Createdd ! " },
       { status: 201 }
     );
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const tickets = await Ticket.find();
+    return NextResponse.json({ tickets }, { status: 200 });
+    return;
   } catch (error) {
     return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
